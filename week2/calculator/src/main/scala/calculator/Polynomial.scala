61caf262e49d9c8eb0479d62639bd2bf582f8919
joaorebelo-ar <joaorebelo.ar@gmail.com>
2015-04-28 21:07:24 +0100

    - polinomial exercise finished.

--- week2/calculator/src/main/scala/calculator/Polynomial.scala
+++ week2/calculator/src/main/scala/calculator/Polynomial.scala
@@ -2,12 +2,27 @@ package calculator
 
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+                   c: Signal[Double]): Signal[Double] = {
+
+    Signal {
+      val na = a()
+      val nb = b()
+      val nc = c()
+      (nb * nb) - 4 * na * nc
+    }
+
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+                       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    Signal {
+      val na = a()
+      val nb = b()
+      val nc = c()
+      val nd = math.sqrt(delta())
+
+      Set((-nb + nd) / (2 * na), (-nb - nd) / (2 * na))
+    }
   }
 }
+  
